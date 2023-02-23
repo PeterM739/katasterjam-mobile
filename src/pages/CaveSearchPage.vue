5a394a618bb256e300aeb7c7883cb37d258ea09f
Matic Di Batista <matic.dibatista@gmail.com>
2023-02-23 15:16:43 +0100

    small cavesearch refactor and new feature for refresh

--- src/pages/CaveSearchPage.vue
+++ src/pages/CaveSearchPage.vue
@@ -1,5 +1,6 @@
 <template>
   <q-input
+    :loading="searching"
     v-model="query"
     debounce="500"
     filled
@@ -7,39 +8,42 @@
     :hint="$t('searchForCave')"
   >
     <template v-slot:append>
-      <q-icon name="search" />
+      <q-icon v-if="query !== ''" name="close" @click="query = ''" class="cursor-pointer" />
+      <q-icon v-else name="search" />
     </template>
     <template v-slot:after>
       <q-btn round dense flat icon="gps_fixed" @click="searchAroundMe" :color="currentSort === 'distance' ? 'light-green' : ''"/>
     </template>
   </q-input>
-  <q-list ref="scrollTargetRef" class="scroll" >
-    <div v-for="(cave, index) in caves" :key="index" >
-      <q-slide-item @left="opt => goTo(opt, cave)">
-        <template v-slot:left>
-          <q-icon name="assist_walker" />
-        </template>
-        <q-item clickable @click="caveClick(cave.caveNumber)">
-          <q-item-section avatar top>
-            <q-avatar icon="info" color="primary" text-color="white" />
-            <q-icon name="keyboard_double_arrow_right" size="lg" color="primary"></q-icon>
-          </q-item-section>
+  <q-pull-to-refresh @refresh="refresh">
+    <q-list ref="scrollTargetRef" class="scroll" >
+      <div v-for="(cave, index) in caves" :key="index" >
+        <q-slide-item @left="opt => goTo(opt, cave)">
+          <template v-slot:left>
+            <q-icon name="assist_walker" />
+          </template>
+          <q-item clickable @click="caveClick(cave.caveNumber)">
+            <q-item-section avatar top>
+              <q-avatar icon="info" color="primary" text-color="white" />
+              <q-icon name="keyboard_double_arrow_right" size="lg" color="primary"></q-icon>
+            </q-item-section>
 
-          <q-item-section>
-            <q-item-label lines="1">{{ cave.caveNumber }} - {{ cave.name }}</q-item-label>
-            <q-item-label caption>{{$t('length')}}: {{ cave.length }} m, {{$t('depth')}}: {{ cave.depth }} m</q-item-label>
-            <q-item-label caption>{{ cave.organization }}</q-item-label>
-            <q-item-label v-if="cave.distance" caption>{{$t('distance')}}: {{ parseInt(cave.distance) }} m</q-item-label>
-          </q-item-section>
+            <q-item-section>
+              <q-item-label lines="1">{{ cave.caveNumber }} - {{ cave.name }}</q-item-label>
+              <q-item-label caption>{{$t('length')}}: {{ cave.length }} m, {{$t('depth')}}: {{ cave.depth }} m</q-item-label>
+              <q-item-label caption>{{ cave.organization }}</q-item-label>
+              <q-item-label v-if="cave.distance" caption>{{$t('distance')}}: {{ parseInt(cave.distance) }} m</q-item-label>
+            </q-item-section>
 
-          <q-item-section side>
-            <q-icon name="map" color="green" size="lg" @click.stop="showOnMapClick(cave)"/>
-          </q-item-section>
-        </q-item>
-      </q-slide-item>
-      <q-separator/>
-    </div>
-  </q-list>
+            <q-item-section side>
+              <q-icon name="map" color="green" size="lg" @click.stop="showOnMapClick(cave)"/>
+            </q-item-section>
+          </q-item>
+        </q-slide-item>
+        <q-separator/>
+      </div>
+    </q-list>
+  </q-pull-to-refresh>
   <q-btn unelevated color="light-blue-7" size="lg" class="full-width" :label="$t('loadMore')" @click="loadMore" :disabled="(totalPages <= pageNumber)"/>
 </template>
 
@@ -88,6 +92,9 @@ export default {
     },
     selectedName () {
       return `[${this.selectedCave?.caveNumber}] ${this.selectedCave?.name}`
+    },
+    searching () {
+      return this.store.getSearchingStatus
     }
   },
   watch: {
@@ -150,47 +157,15 @@ export default {
 
         return
       }
-      if (this.$q.platform.is.cordova) {
-        window.BackgroundGeolocation.getCurrentLocation(async (location) => {
-          this.query = ''
-          this.store.setLocationParameters({
-            ...this.store.getSearchParameters,
-            query: this.query,
-            y: location.latitude,
-            x: location.longitude,
-            sort: 'distance',
-            sortDirection: 'asc',
-            pageNumber: 1
-          })
-
-          await this.store.searchForCaves()
-        }, (code, message) => {
-          console.error('Error when trying to fetch location', code, message)
-        }, {
-          enableHighAccuracy: true
-        })
+      await this.store.searchForNearbyCaves()
+    },
+    async refresh (done) {
+      if (this.currentSort === 'distance') {
+        await this.store.searchForNearbyCaves()
       } else {
-        navigator.geolocation.getCurrentPosition(async (position) => {
-          this.query = ''
-          this.store.setLocationParameters({
-            ...this.store.getSearchParameters,
-            query: this.query,
-            y: position.coords.latitude,
-            x: position.coords.longitude,
-            sort: 'distance',
-            sortDirection: 'asc',
-            pageNumber: 1
-          }, (code, message) => {
-            console.error('Error when trying to fetch location', code, message)
-          }, {
-            enableHighAccuracy: true
-          })
-
-          await this.store.searchForCaves()
-        }, (error) => {
-          console.log(error)
-        })
+        await this.store.searchForCaves()
       }
+      done()
     }
   }
 }
--- src/stores/cave-store.js
+++ src/stores/cave-store.js
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia'
+import { Platform } from 'quasar'
 import { api } from 'src/boot/axios'
 
 export const useCavesStore = defineStore('caves', {
@@ -12,7 +13,8 @@ export const useCavesStore = defineStore('caves', {
       sort: null,
       sortDirection: null
     },
-    totalPages: 0
+    totalPages: 0,
+    searching: false
   }),
 
   getters: {
@@ -33,6 +35,9 @@ export const useCavesStore = defineStore('caves', {
     },
     getCurrentSort (state) {
       return state.searchParameters.sort
+    },
+    getSearchingStatus (state) {
+      return state.searching
     }
   },
 
@@ -53,14 +58,64 @@ export const useCavesStore = defineStore('caves', {
       this.searchParameters.pageNumber++
     },
     async searchForCaves () {
-      const response = await api.get('/api/caves', {
-        params: this.searchParameters
-      })
-      this.totalPages = JSON.parse(response.headers.pagination).totalPages
-      if (this.searchParameters.pageNumber > 1) {
-        response.data.map(cave => this.caves.push(cave))
+      this.searching = true
+      try {
+        const response = await api.get('/api/caves', {
+          params: this.searchParameters
+        })
+        this.totalPages = JSON.parse(response.headers.pagination).totalPages
+        if (this.searchParameters.pageNumber > 1) {
+          response.data.map(cave => this.caves.push(cave))
+        } else {
+          this.caves = response.data
+        }
+      } catch (error) {
+        console.error(error)
+      } finally {
+        this.searching = false
+      }
+    },
+    async searchForNearbyCaves () {
+      if (Platform.is.cordova) {
+        window.BackgroundGeolocation.getCurrentLocation(async (location) => {
+          this.query = ''
+          this.setLocationParameters({
+            ...this.getSearchParameters,
+            query: this.query,
+            y: location.latitude,
+            x: location.longitude,
+            sort: 'distance',
+            sortDirection: 'asc',
+            pageNumber: 1
+          })
+
+          await this.searchForCaves()
+        }, (code, message) => {
+          console.error('Error when trying to fetch location', code, message)
+        }, {
+          enableHighAccuracy: true
+        })
       } else {
-        this.caves = response.data
+        navigator.geolocation.getCurrentPosition(async (position) => {
+          this.query = ''
+          this.setLocationParameters({
+            ...this.getSearchParameters,
+            query: this.query,
+            y: position.coords.latitude,
+            x: position.coords.longitude,
+            sort: 'distance',
+            sortDirection: 'asc',
+            pageNumber: 1
+          }, (code, message) => {
+            console.error('Error when trying to fetch location', code, message)
+          }, {
+            enableHighAccuracy: true
+          })
+
+          await this.searchForCaves()
+        }, (error) => {
+          console.log(error)
+        })
       }
     }
   }
